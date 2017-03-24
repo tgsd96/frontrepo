437967520c9c8ff9e8f9db7b5a5eb836251a2da9
tushargarg <tkg.1996@gmail.com>
2017-03-24 18:30:51 +0530

    production

--- main.bundle.js
+++ main.bundle.js
@@ -800,13 +800,9 @@ var User = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
 var environment = {
-    production: false,
-    api_url: "https://localhost:8080"
+    production: true,
+    api_url: "https`://138.197.75.58:8888"
 };
 //# sourceMappingURL=/Users/tushargarg/Desktop/Web/RadioWebFrontEnd/src/environment.js.map
 
